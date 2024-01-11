650b08a14c9a1b9eee4b155f9d6476ab828e736c
msrdeepu <109930907+msrdeepu@users.noreply.github.com>
2024-01-11 10:10:37 +0530

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,25 +1,34 @@
 import "./bootstrap";
+import "antd/dist/reset.css";
 import "../css/app.css";
+import "antd-css-utilities/utility.min.css";
 
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+import { ConfigProvider } from "antd";
+import theme from "./theme";
+
+const appName =
+    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.jsx`,
-            import.meta.glob("./Pages/**/*.jsx")
-        ),
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        let page = pages[`./Pages/${name}.jsx`];
+        page.default.layout = page.default.layout || ((page) => <>{page}</>);
+        return page;
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: "#4B5563",
+        const app = (
+            <ConfigProvider theme={theme}>
+                <App {...props} />
+            </ConfigProvider>
+        );
+
+        root.render(app);
     },
 });
